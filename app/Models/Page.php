379f8f83b39d9808c5301a2cffe742601f8188db
Marius Fylling <marius@sdok.no>
2025-06-24 22:24:55 +0000

    hasFactory missing

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Page extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'id',
         'event_id',
